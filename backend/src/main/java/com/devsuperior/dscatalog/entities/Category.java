c95f88b9fdfd0e72516e0a5c12656cafc4db141c
Jefferson Fernandes Barreto <jeffinho_fb@hotmail.com>
2021-03-25 21:16:58 -0300

    Pagination

--- backend/src/main/java/com/devsuperior/dscatalog/entities/Category.java
+++ backend/src/main/java/com/devsuperior/dscatalog/entities/Category.java
@@ -20,7 +20,7 @@ public class Category implements Serializable {
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
-	private String Name;
+	private String name;
 	
 	@Column(columnDefinition = "TIMESTAMP WITHOUT TIME ZONE")
 	private Instant createdAt;
@@ -33,7 +33,7 @@ public class Category implements Serializable {
 
 	public Category(Long id, String name) {
 		this.id = id;
-		Name = name;
+		this.name = name;
 	}
 
 	public Long getId() {
@@ -45,11 +45,11 @@ public class Category implements Serializable {
 	}
 
 	public String getName() {
-		return Name;
+		return name;
 	}
 
 	public void setName(String name) {
-		Name = name;
+		this.name = name;
 	}
 
 	public Instant getCreatedAt() {
